bf03a92258b174a3a216c5b2cd1f63af5065d440
sanjeet kumar <sumitkumar989159@gmail.com>
2026-03-27 22:54:05 +0530

    Add files via upload

---
+++ conditional statement/14_leap_year.py
@@ -0,0 +1,6 @@
+print("DHIRAJ JUNGHARE")
+year = int(input("enter the year ="))
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("Leap Year")
+else:
+    print("Not Leap Year")
