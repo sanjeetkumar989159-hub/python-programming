ba59aa36c502bb8505dbe8b2ee9d8165025badbc
sanjeet kumar <sumitkumar989159@gmail.com>
2026-06-14 22:46:13 +0530

    Add files via upload

---
+++ list_problems/10.second_largest.py
@@ -0,0 +1,31 @@
+n = int(input("Enter the size of the list : "))
+
+# Que -> find the second largest element in a list.
+
+if(n <= 1):
+    print("Not possible")
+    exit()
+
+arr = []
+
+for i in range(n):
+    value = int(input("Enter values : "))
+    arr.append(value)
+
+print("The list is : ",arr)
+
+max = float('-inf')
+sec_max = float('-inf')
+
+for i in range(n):
+    if max < arr[i]:
+        sec_max = max
+        max = arr[i]
+    
+    elif sec_max < arr[i] and max != arr[i]:
+        sec_max = arr[i]
+        
+if(sec_max == float('-inf')):
+    print("The second maximum are not present")
+else:
+    print("The second maximum element is : ",sec_max)
